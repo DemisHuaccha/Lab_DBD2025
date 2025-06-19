61ab70cb826c0b68f234fcc45134a095929f5972
Demis <demis.huaccha@usach.cl>
2025-06-18 22:32:31 -0400

    Se agrega la tabla Boleta
    Se agrega la tabla boleta y se corrigen los atributos para ser los mismos que el del MR

--- creacionTablas.sql
+++ creacionTablas.sql
@@ -1,22 +1,31 @@
 CREATE TABLE IF NOT EXISTS Metodo_pago (
-  id_metodo INTEGER PRIMARY KEY,
-  nombre_metodo VARCHAR(30) NOT NULL
+	id_metodo SERIAL PRIMARY KEY,
+	nombre_metodo VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Rol (
-  id_rol INTEGER PRIMARY KEY,
-  nombre_rol  VARCHAR(30) NOT NULL
+	id_rol INTEGER PRIMARY KEY,
+	nombre_rol  VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Usuario (
-  rut_user VARCHAR(10) PRIMARY KEY,
-  correo VARCHAR(30) NOT NULL UNIQUE,
-  clave  VARCHAR(30) NOT NULL,
-  id_rol INTEGER NOT NULL,
-  nombre VARCHAR(30) NOT NULL,
-  rut_administrador VARCHAR(10),
-  CONSTRAINT fk_Usuario_Rol FOREIGN KEY(id_rol) REFERENCES Rol(id_rol)
+	rut_user VARCHAR(15) PRIMARY KEY,
+	correo VARCHAR(30) NOT NULL UNIQUE,
+	clave  VARCHAR(30) NOT NULL,
+	id_rol INTEGER NOT NULL,
+	nombre VARCHAR(30) NOT NULL,
+	rut_user2 VARCHAR(15),
+	CONSTRAINT fk_Usuario_Rol FOREIGN KEY(id_rol) REFERENCES Rol(id_rol)
 );
 
 ALTER TABLE Usuario 
-ADD FOREIGN KEY (rut_administrador) REFERENCES Usuario(rut_user); 
+ADD FOREIGN KEY (rut_user2) REFERENCES Usuario(rut_user); 
+
+CREATE TABLE IF NOT EXISTS Boleta( 
+	numero_boleta SERIAL PRIMARY KEY,
+	fecha DATE NOT NULL,
+	id_metodo SERIAL,
+	rut_user VARCHAR(15),
+	CONSTRAINT fk_Boleta_Usuario FOREIGN KEY (rut_user) REFERENCES Usuario(rut_user),
+	FOREIGN KEY (id_metodo) REFERENCES Metodo_pago(id_metodo)
+);
--- poblacion.sql
+++ poblacion.sql
@@ -19,16 +19,32 @@ VALUES
 	
 /*Tabla de Usuarios*/
 /*Temporal*/
-INSERT INTO Usuario(rut_user,correo,clave,id_rol,nombre,rut_administrador)
+INSERT INTO Usuario(rut_user,correo,clave,id_rol,nombre,rut_user2)
 VALUES
-	('12345678-1','correo1@gmail.com','clave1',1,'nombre1','15948263-1'),
-	('12345678-2','correo2@gmail.com','clave2',1,'nombre2','15948263-1'),
-	('12345678-3','correo3@gmail.com','clave3',1,'nombre3','15948263-1'),
-	('12345678-4','correo4@gmail.com','clave4',1,'nombre4','15948263-1'),
-	('12345678-5','correo5@gmail.com','clave5',1,'nombre5','15948263-1'),
-	('12345678-6','correo6@gmail.com','clave6',1,'nombre6','15948263-1'),
-	('12345678-7','correo7@gmail.com','clave7',1,'nombre7','15948263-1'),
-	('12345678-8','correo8@gmail.com','clave8',2,'nombre8','15948263-1'),
-	('12345678-9','correo9@gmail.com','clave9',2,'nombre9','15948263-1'),
-	('15948263-1','correo10@gmail.com','clave9',3,'nombre10','15948263-1')
+	('12.345.678-1','correo1@gmail.com','clave1',1,'nombre1','12.345.678-1'),
+	('12.345.678-2','correo2@gmail.com','clave2',1,'nombre2','12.345.678-2'),
+	('12.345.678-3','correo3@gmail.com','clave3',1,'nombre3','12.345.678-3'),
+	('12.345.678-4','correo4@gmail.com','clave4',1,'nombre4','12.345.678-4'),
+	('12.345.678-5','correo5@gmail.com','clave5',1,'nombre5','12.345.678-5'),
+	('12.345.678-6','correo6@gmail.com','clave6',1,'nombre6','12.345.678-6'),
+	('12.345.678-7','correo7@gmail.com','clave7',1,'nombre7','12.345.678-7'),
+	('12.345.678-8','correo8@gmail.com','clave8',2,'nombre8','12.345.678-8'),
+	('12.345.678-9','correo9@gmail.com','clave9',2,'nombre9','12.345.678-9'),
+	('15.948.263-1','correo10@gmail.com','clave9',3,'nombre10','15.948.263-1')
 	;
+
+/*Tabla de Boletas*/
+
+INSERT INTO Boleta(numero_boleta,fecha,id_metodo,rut_user)
+VALUES
+	(1,'2025-05-10',1,'12.345.678-1'),
+	(2,'2025-05-10',2,'12.345.678-2'),
+	(3,'2025-05-10',3,'12.345.678-3'),
+	(4,'2025-05-10',2,'12.345.678-4'),
+	(5,'2025-05-10',1,'12.345.678-5'),
+	(6,'2025-05-10',2,'12.345.678-6'),
+	(7,'2025-05-10',3,'12.345.678-7'),
+	(8,'2025-05-10',2,'12.345.678-8'),
+	(9,'2025-05-10',1,'12.345.678-9'),
+	(10,'2025-05-10',3,'12.345.678-5')
+;
