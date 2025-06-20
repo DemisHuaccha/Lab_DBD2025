0fdf3eb678ec83fbdc5517b6b3e3904b224d25d0
Natalia Valdes <123663981+nvo-12@users.noreply.github.com>
2025-06-19 23:16:59 -0400

    Update consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -1,7 +1,16 @@
 /*Consulta 1*/
+-- Agregar una carta al carrito
+INSERT INTO Carrito(cantidad, id_producto, rut_user)
+VALUES (1, 00105, '12.345.678-1'); 
+
+-- Agregar un juego de mesa al carrito
+INSERT INTO Carrito(cantidad, id_producto, rut_user)
+VALUES (1, 00106, '12.345.678-1');
 
 /*Consulta 2*/
-
+-- Eliminar una carta de carrito
+DELETE FROM Carrito
+WHERE id_producto = 00105 AND rut_user = '12.345.678-1';
 /*Consulta 3*/
 
 /*Consulta 4*/
