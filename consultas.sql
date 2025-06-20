d7a2d4920557aa9add21dbbcba90e30c42926ae6
Catalina Angélica Covarrubias Soto <catalina.covarrubias@usach.cl>
2025-06-20 11:29:16 -0400

    Update consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -17,6 +17,7 @@ DELETE FROM Carrito
 WHERE id_producto = 00105 AND rut_user = '12.345.678-1';
 
 /*Consulta 3*/
+-- Mostrar los juegos de mesa / cartas del carrito de compras
 SELECT p.nombre_producto, p.precio_producto, c.cantidad
 FROM Carrito c
 JOIN Producto p ON c.id_producto = p.id_producto
@@ -32,18 +33,21 @@ JOIN Juegos_de_Mesa jm ON p.id_producto = jm.id_producto
 WHERE c.rut_user = '12.345.678-1';
 
 /*Consulta 4*/
+-- Mostrar el precio total a pagar por el carrito de compras.
 SELECT SUM(c.cantidad * p.precio_producto) AS total_a_pagar
 FROM Carrito c
 JOIN Producto p ON c.id_producto = p.id_producto
 WHERE c.rut_user = '12.345.678-1';
 
 /*Consulta 5*/
+-- Mostrar todos los juegos de mesa y cartas que se vendan en una ubicacion geografica especifica.
 SELECT p.id_producto, p.nombre_producto, p.descripcion, p.precio_producto, d.region, t.nombre AS tienda
 FROM Producto p JOIN Cartas c ON p.id_producto = c.id_producto JOIN Producto_Tienda pt ON p.id_producto = pt.id_producto 
 JOIN Tienda t ON pt.id_tienda = t.id_tienda JOIN Usuario u ON t.rut_user = u.rut_user JOIN Direccion d ON u.rut_user = d.rut_user
 WHERE d.region = 'OHiggins';
 
 /*Consulta 6*/
+-- Mostrar ranking de los productos con mas ventas.
 SELECT 
 	p.id_producto,
 	p.nombre_producto,
@@ -58,7 +62,9 @@ SELECT
 FROM Producto p
 ORDER BY p.cantidad_de_ventas DESC
 LIMIT 10;
+
 /*Consulta 7*/
+-- Mostrar lista de deseos de un usuario.
 SELECT
 	u.nombre AS usuario,
 	p.nombre_producto,
